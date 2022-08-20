d3d8fa0723cf670fe486febc6453d4983a7726bb
adrianvengeance <moh.adrian11@gmail.com>
2022-08-20 23:06:49 +0700

    make dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+// i added
+namespace App\Http\Controllers\Admin;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+/**
+ * route for admin
+ */
+//group route with prefix "admin"
+Route::prefix('admin')->group(function () {
+    //group route with middleware "auth"
+    Route::group(['middleware' => 'auth'], function () {
+        //route dashboard
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard.index');
+    });
 });
