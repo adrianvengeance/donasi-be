95e7ba7b81aa1756004d7da5dcb55bfb3f2584ea
adrianvengeance <moh.adrian11@gmail.com>
2022-09-04 13:32:06 +0700

    restful api login

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 /**
  * Api Register
  */
 Route::post('/register', [RegisterController::class, 'register']);
+
+/**
+ * Api Login
+ */
+Route::post('/login', [LoginController::class, 'login']);
